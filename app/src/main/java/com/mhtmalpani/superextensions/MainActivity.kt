9dee5f43f21d50677b605f8272f0af6d8946be70
Mohit Malpani <androidwithoutwifi@gmail.com>
2020-06-20 17:30:14 +0530

    Fix gradle issue

--- app/src/main/java/com/mhtmalpani/superextensions/MainActivity.kt
+++ app/src/main/java/com/mhtmalpani/superextensions/MainActivity.kt
@@ -1,32 +1,12 @@
 package com.mhtmalpani.superextensions
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.mhtmalpani.superextensions.view.afterTextChanged
-import com.mhtmalpani.superextensions.view.bindView
-import com.mhtmalpani.superextensions.view.onTextChanged
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val title by bindView<TextView>(R.id.title)
-    private val subTitle: TextView by bindView(R.id.subTitle)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        custom()
-    }
-
-    private fun custom() {
-        editText.afterTextChanged {
-            println(it)
-        }
-
-        editText.onTextChanged { charSequence, start, before, after ->
-            println("$charSequence, $start, $before, $after")
-        }
     }
 }
