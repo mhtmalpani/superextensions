cefc9de9230d9e8ac1aca77f6cc938524354da6b
Mohit Malpani <androidwithoutwifi@gmail.com>
2020-07-11 01:30:25 +0530

    Add Asset helper

--- extensionsexample/src/main/java/com/mhtmalpani/extensionsexample/MainActivity.kt
+++ extensionsexample/src/main/java/com/mhtmalpani/extensionsexample/MainActivity.kt
@@ -2,11 +2,32 @@ package com.mhtmalpani.extensionsexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mhtmalpani.extensionsexample.model.LibraryModel
+import com.mhtmalpani.superextensions.custom.assetToJsonObject
+import com.mhtmalpani.superextensions.custom.readAssetAsString
+import com.mhtmalpani.superextensions.view.bindStringArgument
 
 class MainActivity : AppCompatActivity() {
+
+    private val data by bindStringArgument(DATA)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        assetHelper()
+    }
+
+    private fun assetHelper() {
+        val fileAsString = readAssetAsString("sample.json")
+        println(fileAsString)
+
+        val fileAsObject = assetToJsonObject("sample.json", LibraryModel::class.java)
+        println("Name: ${fileAsObject.name}")
+        println("Type: ${fileAsObject.type}")
+    }
+
+    companion object {
+        private const val DATA = "data"
     }
 }
