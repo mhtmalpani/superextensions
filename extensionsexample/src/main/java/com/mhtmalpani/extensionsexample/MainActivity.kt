f3d6c51c76e5bbc52bfb916d9d6555944612c7a4
Mohit Malpani <androidwithoutwifi@gmail.com>
2020-07-14 01:08:00 +0530

    Add extra string extensions

--- extensionsexample/src/main/java/com/mhtmalpani/extensionsexample/MainActivity.kt
+++ extensionsexample/src/main/java/com/mhtmalpani/extensionsexample/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.mhtmalpani.extensionsexample.model.LibraryModel
 import com.mhtmalpani.superextensions.custom.assetToJsonObject
 import com.mhtmalpani.superextensions.custom.readAssetAsString
+import com.mhtmalpani.superextensions.primitive.convertToTitleCase
+import com.mhtmalpani.superextensions.primitive.encodeToUTF8
+import com.mhtmalpani.superextensions.primitive.sha256
+import com.mhtmalpani.superextensions.primitive.sha512
 import com.mhtmalpani.superextensions.view.bindStringArgument
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         assetHelper()
+        stringExtensions()
     }
 
     private fun assetHelper() {
@@ -25,6 +30,18 @@ class MainActivity : AppCompatActivity() {
         val fileAsObject = assetToJsonObject("sample.json", LibraryModel::class.java)
         println("Name: ${fileAsObject.name}")
         println("Type: ${fileAsObject.type}")
+    }
+
+    private fun stringExtensions() {
+        val sha = "Hello".sha512()
+        println("Hash of Hello: $sha")
+
+        val url = "https://www.example.com?ref=www.github.com&hash=123456789"
+        println("UTF-8 encoded: ${url.encodeToUTF8()}")
+
+        val sentence = "This is an example sentence for string title case"
+        println(sentence.convertToTitleCase())
+        println(sentence.convertToTitleCase(listOf("for", "is", "an")))
     }
 
     companion object {
